6b666415345f05b64cf2e2be7eb4e95a02164860
l1l7y6 <164134381+l1l7y6@users.noreply.github.com>
2024-05-20 15:04:45 +0800

    Add files via upload

---
+++ Practical4/recursive.py
@@ -0,0 +1,4 @@
+X=True
+Y=False
+W=(X or Y) and not (X and Y)
+print(W)
---
+++ Practical4/variables.py
@@ -0,0 +1,12 @@
+a=40
+b=36
+c=30
+d=a-b
+e=b-c
+print(d)
+print(e)
+if d>e:
+  print("d is bigger")
+if d<e:
+  print("e is bigger")
+#the second training regime had a greater improvement on runnning time.
